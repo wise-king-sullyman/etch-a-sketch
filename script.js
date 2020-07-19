048f5f0526b6e82ebbfdac34e0b425a399789c03
wise-king-sullyman <asullivan96@live.com>
2020-07-18 23:45:24 -0400

    Added additional coloring modes

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let boardSideLength = 16;
-grid = document.getElementById("grid");
+let grid = document.getElementById("grid");
+let modeSelector = 0; //0 for b&w mode, 1 for rainbow, 2 for greyscale
 
 function createSquares(appendToID) {
     for(let i=0; i < boardSideLength; i++) {
@@ -12,7 +13,7 @@ function createSquares(appendToID) {
     }
 };
 
-function createColumns(appendToID) {
+function createGrid(appendToID) {
     for(let i=0; i < boardSideLength; i++) {
         let column = document.createElement("DIV");
         column.className = "column";
@@ -26,7 +27,8 @@ function createColumns(appendToID) {
 
 function getNewBoardSize() {
     newBoardSize = prompt("Enter number for new board size, e.g. 16 will create a 16x16 board. Sizes over 100 start to get laggy");
-    !newBoardSize? alert("Enter number greater than 0"): resizeBoard()
+    !newBoardSize || isNaN(newBoardSize)? 
+    alert("Enter number greater than 0"): resizeBoard()
 }
 
 function resizeBoard() {
@@ -37,20 +39,58 @@ function resizeBoard() {
     grid = document.createElement("DIV");
     grid.id = "grid"
     document.getElementById("container").appendChild(grid)
-    createColumns("grid")
-    addEventListeners()
-}
-
-function addEventListeners() {
-    let squares = document.querySelectorAll(".square");
-    squares.forEach((square) => {
-        square.addEventListener("mouseenter", function(e) {
-            e.target.style.backgroundColor = "black";
-        });
-});
-    let resetButton = document.getElementById('resetButton');
-    resetButton.addEventListener("click", getNewBoardSize);
+    createGrid("grid")
+    addSquareEventListeners()
 };
 
-createColumns("grid")
-addEventListeners()
+function colorSquare(e) {
+    if (modeSelector === 0) {
+        e.target.style.backgroundColor = "black";
+    }else if (modeSelector === 1) {
+        e.target.style.backgroundColor = randomColorGenerator();
+    }else if (modeSelector === 2) {
+        e.target.style.backgroundColor = darkenBackground(e);
+    }
+    
+};
+
+function addSquareEventListeners() {
+    let squares = document.querySelectorAll(".square");
+    squares.forEach((square) => {
+        square.addEventListener("mouseenter", colorSquare);
+    });
+};
+
+function randomColorGenerator() {
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+    return `rgb(${r},${g},${b})`
+}
+
+function darkenBackground(e) {
+    let currentSquareColor = window.getComputedStyle(e.target).getPropertyValue("background-color");
+    console.log(currentSquareColor)
+    let currentAlpha = currentSquareColor.slice(-4,-1);
+    let newAlpha;
+    if(currentAlpha == 255){newAlpha = 0.9}
+    else {newAlpha = ((currentAlpha*10)-1)/10}
+    return `rgba(255,255,255,${newAlpha})`
+};
+
+let resetButton = document.getElementById('resetButton');
+resetButton.addEventListener("click", getNewBoardSize);
+
+let blackAndWhiteModeButton = document.getElementById('blackMode');
+blackAndWhiteModeButton.addEventListener("click", function(){modeSelector=0});
+
+let rainbowModeButton = document.getElementById('rainbowMode');
+rainbowModeButton.addEventListener("click", function(){modeSelector=1});
+
+let greyscaleModeButton = document.getElementById('greyscaleMode');
+greyscaleModeButton.addEventListener("click", function(){modeSelector=2});
+
+
+
+createGrid("grid")
+addSquareEventListeners()
